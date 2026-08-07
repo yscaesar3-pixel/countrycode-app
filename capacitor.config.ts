06ddd64a3a3556b6da63185714ed639647f25102
Yuta Seki <sekiyubusi@gmail.com>
2026-08-07 19:14:56 +0900

    Use capacitor.config.json instead of .ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.yutaXXX.countrycode',
-  appName: '国コード早見帳',
-  webDir: 'www'
-};
-
-export default config;
